7a15e802172fa2a8a0a284bf45156cc58ab8f74d
Hayat Jemal <htjemal2000@gmail.com>
2022-06-21 05:41:00 +0300

    python

---
+++ python/exercise1.py
@@ -0,0 +1,22 @@
+# print("Hello, world")
+
+# input("what is your name? ")
+
+# miles = input("How many miles do you want to convert into kilometers? ")
+# print(miles)
+
+# print(type(miles))
+# km = miles*1.609
+# print("The miles in km is =: " + km)
+
+name=input("what is your name? ")
+
+print("Hi, " + name + "! Nice to meet you")
+
+age = input(name + " how old are you? ")
+
+print(type(age))
+
+yob = 2022-int(age)
+
+print(name + ", you were born in " + str(yob))
